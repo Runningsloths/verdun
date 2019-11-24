fb344c1824f42bcf35f406f427348de22c548ad1
Tony Yeon <tony.yeon8310@gmail.com>
2019-11-23 18:49:14 -0800

    k

--- VuforiaTest/Assets/Scripts/Rotate.cs
+++ VuforiaTest/Assets/Scripts/Rotate.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerRotate : MonoBehaviour
+public class Rotate : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class playerRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(0,Time.deltaTime*15,0));
     }
 }
